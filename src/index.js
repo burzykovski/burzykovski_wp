33033a6002533fe3b67d259fbf7a970aa90fe458
burzykovski <burzyk.mb@gmail.com>
2018-02-13 22:35:32 +0100

    setting up the project | react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'WP RECRUITMENT TASK';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+
+module.hot.accept();
