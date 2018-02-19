56894dbebe8c978e27c060606fba07e619f0a460
burzykovski <burzyk.mb@gmail.com>
2018-02-19 19:59:14 +0100

    first implementation of Masonry Layout in Collection

--- src/scenes/Collection.js
+++ src/scenes/Collection.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Col, Row, Image } from "react-bootstrap";
+import Masonry from "react-masonry-component";
 
 class Collection extends Component {
-  render() {
-    return (
-      <div>
-        <p>id to: {this.props.match.url}</p>
-      </div>
-    )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: []
+    };
   }
-};
+
+  actualUrl = this.props.match.url;
+  collectionID = this.actualUrl.slice(this.actualUrl.lastIndexOf("/") + 1);
+
+  componentDidMount() {
+    const API_URL = `data/${this.collectionID}.json`;
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => this.setState({ photos: data }));
+  }
+  render() {
+    const masonryImages = this.state.photos.map(function(img) {
+      return (
+        <Col xs={12} sm={6} md={4} key={img.id}>
+        <Image
+          className="masonry-image"
+          key={img.id}
+          src={img.urls.small}
+          alt={img.description}
+          responsive
+        />
+        </Col>
+      );
+    });
+
+    return <Masonry>{masonryImages}</Masonry>;
+  }
+}
 
 export default Collection;
