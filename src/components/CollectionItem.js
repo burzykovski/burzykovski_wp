d4445c5d571fe48d5740e9b86818d0aff92a8a96
burzykovski <burzyk.mb@gmail.com>
2018-02-20 12:55:12 +0100

    add More Info Popovers to photos in Collection | show likes, downloads and country as More Info

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -1,15 +1,58 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
+import { Image, Popover, OverlayTrigger, Label } from "react-bootstrap";
 
 class CollectionItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoInfo: []
+    };
+  }
+
+  API_URL = `https://api.unsplash.com/photos/${this.props.imgId}?client_id=9e7a4bba4e2738e9e1aa9c34c4aa9433a10d037cd5b56a97c67a0e159a65b3fd`;
+
+  handleMouseOver = () => {
+    this.state.photoInfo.length === 0 ? this.fetchPhotoInfo() : null;
+  };
+
+  extractPhotoInfo = data => {
+    return new Promise((resolve) => {
+      resolve({ likes: data.likes, downloads: data.downloads, country: (data.location === undefined ? "no info" : data.location.country) });
+    });
+  };
+
+  fetchPhotoInfo = () => {
+    fetch(this.API_URL)
+      .then(response => response.json())
+      .then(data => this.extractPhotoInfo(data))
+      .then(data => this.setState({ photoInfo: data }));
+  }
+
+
   render() {
+    const {likes, downloads, country} = this.state.photoInfo;
+    const popoverHoverFocus = (
+      <Popover id="popover-trigger-hover-focus" title="More info">
+        <Label>Likes:</Label> {likes}<br/>
+        <Label>Downloads:</Label> {downloads}<br/>
+        <Label>Country:</Label> {country}
+      </Popover>
+    );
     return (
-      <Image
-        className="masonry-image"
-        src={this.props.imgSrc}
-        alt={this.props.imgAlt}
-        responsive
-      />
+      <OverlayTrigger
+        trigger={["hover", "focus"]}
+        placement="right"
+        overlay={popoverHoverFocus}
+      >
+        <Image
+          className="masonry-image"
+          src={this.props.imgSrc}
+          alt={this.props.imgAlt}
+          responsive
+          onMouseOver={this.handleMouseOver}
+        />
+      </OverlayTrigger>
     );
   }
 }
